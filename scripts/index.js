1ae28d7f2979c08ecca72e6dc65c54a64accc686
Tanner M <matatm@outlook.com>
2023-06-01 12:15:08 -0400

    Project 4 Stage 1, Revise 3

--- scripts/index.js
+++ scripts/index.js
@@ -22,7 +22,7 @@ const initialCards = [
 {
     title: "Lago di Braies",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
-}
+},
 ];
  
 console.log(initialCards);
